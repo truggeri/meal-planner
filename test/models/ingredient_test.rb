79ea5d36da800f75e332c706d35e3117a88ef7ff
Thomas Ruggeri <truggeri@gmail.com>
2019-03-23 18:54:46 -0400

    Fixing ingredient test

--- test/models/ingredient_test.rb
+++ test/models/ingredient_test.rb
@@ -17,15 +17,15 @@ class IngredientTest < ActiveSupport::TestCase
     assert_not @ingredient.valid?
   end
 
-  test "name should be shorter than 64 characters" do
+  test "name should be shorter than 50 characters" do
     o = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
-    @ingredient.name = (0...65).map { o[rand(o.length)] }.join
+    @ingredient.name = (0...51).map { o[rand(o.length)] }.join
     assert_not @ingredient.valid?
   end
 
-  test "name can be 64 characters" do
+  test "name can be 50 characters" do
     o = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
-    @ingredient.name = (0...64).map { o[rand(o.length)] }.join
+    @ingredient.name = (0...50).map { o[rand(o.length)] }.join
     assert @ingredient.valid?
   end
 
@@ -43,15 +43,15 @@ class IngredientTest < ActiveSupport::TestCase
     assert_not @ingredient.valid?
   end
 
-  test "description should be shorter than 255 characters" do
+  test "description should be shorter than 200 characters" do
     o = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
-    @ingredient.description = (0...256).map { o[rand(o.length)] }.join
+    @ingredient.description = (0...201).map { o[rand(o.length)] }.join
     assert_not @ingredient.valid?
   end
 
-  test "description can be 255 characters" do
+  test "description can be 200 characters" do
     o = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
-    @ingredient.description = (0...255).map { o[rand(o.length)] }.join
+    @ingredient.description = (0...200).map { o[rand(o.length)] }.join
     assert @ingredient.valid?
   end
 
