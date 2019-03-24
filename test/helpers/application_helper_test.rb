f502bfe78eb3f173775c9d1da9b6dd71091a2b6a
Thomas Ruggeri <truggeri@gmail.com>
2019-03-23 22:05:35 -0400

    Adding app helper test

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full_title default" do
+    assert_equal full_title(), "Meal Planner"
+  end
+
+  test "full_title with parameter" do
+    phrase = FFaker::Lorem.words(3).join(" ").to_s
+    assert_equal full_title(phrase), "#{phrase} | Meal Planner"
+  end
+end
