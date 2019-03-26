cb3b820326c19b3ed0c0961954dd0d625a29d193
Thomas Ruggeri <truggeri@gmail.com>
2019-03-26 16:52:30 -0400

    Trying to fix coverall coverage report

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,18 @@
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!('rails')
+# NOTE-truggeri-20190323 Exempting these as there's no code in them yet
+EXEMPT_CLASSES = %w[channels jobs mailers].freeze
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+])
+SimpleCov.start 'rails' do
+  EXEMPT_CLASSES.each { |dir| add_filter "app/#{dir}/" }
+end
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
-
-require 'simplecov'
-# NOTE-truggeri-20190323 Exempting these as there's no code in them yet
-EXEMPT_CLASSES = %w[channels jobs mailers].freeze
-SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter])
-SimpleCov.start 'rails' do
-  EXEMPT_CLASSES.each { |dir| add_filter "app/#{dir}/" }
-end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
