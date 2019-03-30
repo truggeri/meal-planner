d0c80d3f462876d3aa5fe291e64e95a93837e3a7
Thomas Ruggeri <truggeri@gmail.com>
2019-03-30 15:20:25 -0400

    Adding current_user before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+
+  def set_current_user
+    # TODO-truggeri-20190330
+    #   Statically set to only user for now, more to come later
+    @current_user = User.find_by(id: 1)
+  end
 end
