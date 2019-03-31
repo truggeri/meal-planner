c371773c638dd90726f27b961d3be51d2edfa1df
Thomas Ruggeri <truggeri@gmail.com>
2019-03-31 18:38:06 -0400

    Loading simplecov before devise can load models

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,3 @@
-require 'simplecov'
-require 'coveralls'
-# NOTE-truggeri-20190323 Exempting these as there's no code in them yet
-EXEMPT_CLASSES = %w[channels jobs mailers].freeze
-SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-])
-SimpleCov.start 'rails' do
-  EXEMPT_CLASSES.each { |dir| add_filter "app/#{dir}/" }
-end
-
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
