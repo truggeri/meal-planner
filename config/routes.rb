f4a1b167ad13902524f50ce9087ebb2d9fe3b493
Thomas Ruggeri <truggeri@gmail.com>
2019-04-13 19:21:55 -0400

    Account creation on User creation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
   root 'static_pages#home'
 
   resources :ingredients, only: %i[index show new create destroy]
