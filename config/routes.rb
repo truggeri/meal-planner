112efdb60ae9ffcde643575887753c2207e05d62
Thomas Ruggeri <truggeri@gmail.com>
2019-09-29 17:13:15 -0400

    Performing upgrade rake task

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: "users/registrations"
-  }
-  root 'static_pages#home'
-
-  resources :ingredients, only: %i[index show new create destroy]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
