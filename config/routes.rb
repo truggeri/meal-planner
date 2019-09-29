1896b341cca3c416182385ff4bc7978518af1885
Thomas Ruggeri <truggeri@gmail.com>
2019-09-29 17:28:08 -0400

    Cleaning up issues since upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+      registrations: "users/registrations"
+  }
+  root 'static_pages#home'
+
+  resources :ingredients, only: %i[index show new create destroy]
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,14 +3,12 @@
 #  Abandoning for now as it's more trouble than it's worth. Issues _related_ to,
 #  https://github.com/colszowka/simplecov/issues/599
 
-if Rails.env.test?
-  require "simplecov"
-  # NOTE-truggeri-20190323 Exempting these as there's no code in them yet
-  EXEMPT_CLASSES = %w[channels jobs mailers].freeze
-  SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter])
-  SimpleCov.start 'rails' do
-    EXEMPT_CLASSES.each { |dir| add_filter "app/#{dir}/" }
-  end
+require "simplecov"
+# NOTE-truggeri-20190323 Exempting these as there's no code in them yet
+EXEMPT_CLASSES = %w[channels jobs mailers].freeze
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter])
+SimpleCov.start 'rails' do
+  EXEMPT_CLASSES.each { |dir| add_filter "app/#{dir}/" }
 end
 
 ENV['RAILS_ENV'] ||= 'test'
